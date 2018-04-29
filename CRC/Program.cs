84ff36e9b00610ca6d955434b92b5fadb0f310a1
Gao Fei <gaofeibuaa@outlook.com>
2018-04-29 22:21:57 +0800

    Fix: fix a bug.

--- CRC/Program.cs
+++ CRC/Program.cs
@@ -24,24 +24,32 @@ namespace CRCTEST
 
                 dat = new byte[n];
 
-                Console.Write("请输入CRC校验数据(以空格分隔)：");
+                Console.WriteLine("请输入CRC校验数据(十六进制，以空格分隔)：");
                 string[] strs = Console.ReadLine().Split(' ');
                 if (strs.Length != n)
                 {
                     Console.WriteLine("输入数据与数组长度不符，请重新输入...\n");
                     continue;
                 }
+
                 //转换数据至byte[] dat
-                for (int i = 0; i < n; i++)
+                try
                 {
-                    dat[i] = Convert.ToByte(strs[i], 16);
+                    for (int i = 0; i < n; i++)
+                    {
+                        dat[i] = Convert.ToByte(strs[i], 16);
+                    }
+                } catch(Exception)
+                {
+                    Console.WriteLine("输入的校验数据必须是十六进制数字...\n");
+                    continue;
                 }
 
                 crc.crcCheck(dat, n);
                 Console.WriteLine("CRC校验结果为:");
                 Console.WriteLine("LowByte: 0x{0:X}", crc.LowByte);
                 Console.WriteLine("HighByte: 0x{0:X}", crc.HighByte);
-                Console.Write("---------------------");
+                Console.Write("-----------------------------------------");
 
                 ////输出数据
                 //Console.Write("输入的数据是:");
